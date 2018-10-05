bc17a57e926eb5e2458039720bad43f5934c427c
Juan Aguillon <miloca.98@hotmail.com>
2018-10-05 00:19:03 -0500

    Añadiendo Toolbar

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,13 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialDemoComponent } from './material-demo/material-demo.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @NgModule({
   imports: [
-    CommonModule
-    
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ],
-  declarations: [MaterialDemoComponent], 
-  exports:[ MaterialDemoComponent ]
+  declarations: [
+    MaterialDemoComponent,
+
+  ], 
+  exports:[ 
+    MatToolbarModule
+   ]
 })
 export class MaterialModule { }
